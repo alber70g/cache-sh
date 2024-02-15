5a5b42be07d5232288cff01fe0fd03f602504d7f
Albert G <516972+alber70g@users.noreply.github.com>
2024-02-15 11:25:47 +0100

    change execution of command from `spawn` to `exec`

--- src/utils/createRunCommand.ts
+++ src/utils/createRunCommand.ts
@@ -1,22 +1,17 @@
-import { spawn } from 'child_process';
+import { exec } from 'child_process';
 
 export const createRunCommand = (command: string, cwd: string) => async () => {
   return new Promise((resolve, reject) => {
     try {
-      const [cmd, ...cmdArgs] = command.split(' ');
-      const child = spawn(cmd, cmdArgs, { cwd });
-
-      child.stdout.on('data', (data: any) => {
-        process.stdout.write(data);
-      });
-      child.stderr.on('data', (data: any) => {
-        process.stderr.write(data);
-      });
-      child.on('error', (error: any) => {
-        console.error(error);
-      });
-      child.on('exit', (code: any) => {
-        resolve(code);
+      exec(command, { cwd }, (error, stdout, stderr) => {
+        if (error) {
+          console.error(error);
+          reject(error);
+        } else {
+          process.stdout.write(stdout);
+          process.stderr.write(stderr);
+          resolve(0);
+        }
       });
     } catch (e) {
       reject('unexpected error occurred' + e);
